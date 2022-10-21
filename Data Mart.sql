f11def5e316bcc38a32db6af99b5bc51d6aead6b
SenaaKa <112318210+SenaaKa@users.noreply.github.com>
2022-10-22 01:17:19 +0300

    Update Data Mart.sql

--- Data Mart.sql
+++ Data Mart.sql
@@ -1,33 +1,56 @@
+--What day of the week is used for each week_date value?
+select distinct to_char(week_date, 'day')  from clean_weekly_sales cws 
 
 
-create table clean_weekly_sales as (select
-  to_date(week_date, 'DD/MM/YY') AS week_date, 
-  date_part('week', to_date(week_date, 'DD/MM/YY')) AS week_number,
-  date_part('month', to_date(week_date, 'DD/MM/YY')) AS month_number,
-  date_part('year', to_date(week_date, 'DD/MM/YY')) AS calendar_year,
-region,
-platform,
-segment,
-customer_type,
-transactions,
-sales,  
-  case 
-  	when segment like '%1%' then 'Young Adults'
-  	when segment like '%2%' then 'Middle Age'
-  	when segment like '%3%' or segment like '%4%' then 'Retiress'
-  	else 'Unknown'
-  end as age_band,
-  case 
-  	when left (segment,1)='C' then 'Couples'
-  	when left (segment,1)='F' then 'Families'
-  	else 'Unknown'
-  end as demographic,
-  round((sales::numeric/transactions),2) as avg_transaction
-  from weekly_sales)
 
 
-  
-  
-  
-  
-  
+
+--What range of week numbers are missing from the dataset?
+select distinct week_number from clean_weekly_sales cws 
+order by week_number 
+
+
+
+
+--How many total transactions were there for each year in the dataset?
+select calendar_year,  count(transactions) total_transactions  from clean_weekly_sales cws 
+group by calendar_year 
+order by calendar_year 
+
+
+--What is the total sales for each region for each month?
+select month_number, sum(sales), region from clean_weekly_sales cws 
+group by region, month_number 
+order by region
+
+
+
+--What is the total count of transactions for each platform
+select platform, sum(transactions) from clean_weekly_sales cws 
+group by platform 
+
+
+
+--What is the percentage of sales for Retail vs Shopify for each month?
+
+
+
+
+
+--What is the percentage of sales by demographic for each year in the dataset?
+
+
+
+
+--Which age_band and demographic values contribute the most to Retail sales?
+
+
+
+--Can we use the avg_transaction column to find the average transaction size for each year for Retail vs Shopify? If not - how would you calculate it instead?
+
+
+
+
+
+
+
